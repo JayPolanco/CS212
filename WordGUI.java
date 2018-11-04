9f9292d07eaf1f1e3663b70815b7b872e9c5215f
JayPolanco <44456195+JayPolanco@users.noreply.github.com>
2018-11-04 12:02:18 -0500

    Add files via upload

---
+++ WordGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+public class WordGUI extends JFrame {
+   
+   public static void initialize(){
+      
+      myWordGUI = new WordGUI();
+      myWordGUI.setSize(400,200);
+      
+      
+   }
+
+}
---
+++ word.java
@@ -0,0 +1,10 @@
+
+public class word {
+   static int WORD_SIZE = 3;
+   private String words;
+   
+   //Constructor
+   public word(String w){
+      words = w;
+   }
+}
