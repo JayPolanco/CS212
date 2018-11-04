82191875dcb8ab8c59922bc956d527fe9a829de1
JayPolanco <44456195+JayPolanco@users.noreply.github.com>
2018-11-04 15:57:14 -0500

    Update Project3.java

--- Project3/Project3.java
+++ Project3/Project3.java
@@ -1,6 +1,7 @@
 /**
  * This Program takes in a text file of words selected by the user from a file menu option 
- * and puts the words in sorted and unsorted linked lists. Then prints the 
+ * and puts the words in sorted and unsorted linked lists, which
+ * are there own classes. Then prints the 
  * unsorted and sorted list of words onto a GUI. 
  * @author Jason Polanco 
  *
